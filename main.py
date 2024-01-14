e49b47887815971069ca6691e6f81fb7b3e5f67c
coutinho <provezano@gmail.com>
2024-01-14 18:42:43 -0300

    data validation

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from ml_project import logger
 from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ml_project.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+
 
 STAGE_NAME = 'Data Ingestion Stage'
         
-if __name__ == '__main__':
-    try:
-        logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<')
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<')
-    except Exception as e:
-        logger.exception(e)
+try:
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<')
+except Exception as e:
+    logger.exception(e)
+
+
+STAGE_NAME = 'Data Validation Stage'
+
+try:
+    logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<')
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<')
+except Exception as e:
+    logger.exception(e)
