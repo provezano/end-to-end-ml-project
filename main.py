474262c9790f31b4f64df4fc720631631d00134e
coutinho <provezano@gmail.com>
2024-01-14 18:41:27 -0300

    ups

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from ml_project import logger
+from ml_project.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info('Welcome!')
-
+STAGE_NAME = 'Data Ingestion Stage'
+        
+if __name__ == '__main__':
+    try:
+        logger.info(f'>>>>> stage {STAGE_NAME} started <<<<<')
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f'>>>>> stage {STAGE_NAME} completed <<<<<')
+    except Exception as e:
+        logger.exception(e)
